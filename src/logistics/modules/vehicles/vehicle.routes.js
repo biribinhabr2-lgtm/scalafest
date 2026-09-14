5bd33972b5ddfbb65bf2d68ceb34fc9b59daf1df
biribinhabr2-lgtm <biribinhabr2@gmail.com>
2026-09-14 17:22:39 -0300

    Delete src directory

--- src/logistics/modules/vehicles/vehicle.routes.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const { Router } = require('express');
-const c = require('./vehicle.controller');
-const r = Router();
-r.get('/', c.list); r.get('/:id', c.get); r.post('/', c.create); r.put('/:id', c.update); r.delete('/:id', c.remove);
-module.exports = r;
